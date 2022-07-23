b23386ae2e2e5bb4002867794a4bc0380026c6eb
shes1996 <shes1996@bk.ru>
2022-07-23 11:02:02 +0300

    count of number

--- Program.cs
+++ Program.cs
@@ -2,9 +2,13 @@
 14212 -> нет
 12821 -> да
 23432 -> да*/
-number = Convert.ToInt32 (Console.Readline());
-lenght = Lenght.string(number);
-if (lenght == 5 && number[1] != "-")
+int number = Convert.ToInt32 (Console.ReadLine ());
+int lenght = (int)(Math.Log10(Math.Abs(number))+1);
+Console.WriteLine(lenght);
+/*if (lenght == 5 && number[1] != "-")
 {
-    
-}
+    for (int i = 0; i < lenght-1; i++)
+    {
+        if (number[i] == number[lenght-1-i]) Console.WriteLine("Число является палиндромом");
+    }
+}*/
